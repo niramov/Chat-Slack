4491edaa5313da7bc7fd9a57632d51d86c20ff9c
Nikita Abramov <nikkita.abramov@gmail.com>
2023-09-14 18:12:37 +0200

    step 3.5

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import routes from '../routes/routes';
+import axios from 'axios';
+import getAuthHeader from '../utils/getAuthHeader';
 
 const MainPage = () => {
+  const [chatData, setChatData] = useState(null);
   const navigate = useNavigate();
+
   useEffect(() => {
     const userId = JSON.parse(localStorage.getItem('userId'));
     if (userId === null) {
       navigate('/login');
     }
+    const getData = async () => {
+      try {
+        const headers = getAuthHeader();
+        const response = await axios.get(routes.usersPath(), { headers });
+        setChatData(response);
+      } catch (error) {
+        console.error('Ошибка авторизации', error);
+      }
+    };
+    getData();
+    console.log('chatData', chatData);
   });
 
   return <div>Страница чата</div>;
---
+++ frontend/src/utils/getAuthHeader.js
@@ -0,0 +1,11 @@
+const getAuthHeader = () => {
+  const user = JSON.parse(localStorage.getItem('userId'));
+
+  if (user && user.token) {
+    console.log('token', user.token);
+    return { Authorization: `Bearer: ${user.token}` };
+  }
+  return {};
+};
+
+export default getAuthHeader;
