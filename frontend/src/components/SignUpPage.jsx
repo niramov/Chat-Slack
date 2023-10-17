b5752a8cb7ea8b09e16c31fe178c3b1df00af084
Nikita Abramov <nikkita.abramov@gmail.com>
2023-10-17 17:41:50 +0200

    fixes

--- frontend/src/components/SignUpPage.jsx
+++ frontend/src/components/SignUpPage.jsx
@@ -5,7 +5,7 @@ import SignUpForm from './SignUpForm.jsx';
 const SignUpPage = () => {
   return (
     <Container fluid className='h-100 m-3'>
-      <Row className=' justify-content-center align-items-center h100'>
+      <Row className='justify-content-center align-items-center h100'>
         <Card style={{ width: '30rem' }} className='text-center shadow-sm'>
           <Card.Body className='row'>
             <SignUpForm />
